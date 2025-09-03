a5919153bd165af301cee16cd9903eb708fc5289
ShubhamS79 <shankarsawant670@gmail.com>
2025-09-03 10:59:58 +0530

    initial commit

--- gitProject/src/gitProject/First.java
+++ gitProject/src/gitProject/First.java
@@ -3,7 +3,7 @@ package gitProject;
 public class First {
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("this is first project ");
+		System.out.println("this is p ");
 	}
 
 }
