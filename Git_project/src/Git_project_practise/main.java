e8836e6e4e209aac9967fd285bb4f7fdf95afb87
ShubhamS79 <shankarsawant670@gmail.com>
2025-09-03 10:09:51 +0530

    Delete Git_project directory

--- Git_project/src/Git_project_practise/main.java
+++
@@ -1,8 +0,0 @@
-package Git_project_practise;
-
-public class main {
-	public static void main(String[] args) {
-		System.out.println("hello world");
-	}
-
-}
--- Git_project/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module Git_project {
-}
